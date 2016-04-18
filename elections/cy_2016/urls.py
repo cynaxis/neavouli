d7813adcbbbc97921d28589a3fc77801c924f9a3
wfdd <erdbanfed@gmail.com>
2016-04-18 14:26:08 +0300

    Add contact page in CY app

--- elections/cy_2016/urls.py
+++ elections/cy_2016/urls.py
@@ -1,4 +1,6 @@
+
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from candidates.views.help import HelpAboutView, HelpApiView
 from .views import DistrictSelectorView
@@ -11,4 +13,7 @@ urlpatterns = [
     url(r'^help/api',
         HelpApiView.as_view(template_name='cy_2016/api.html'),
         name='help-api'),
+    url(r'^help/contact',
+        TemplateView.as_view(template_name='cy_2016/contact.html'),
+        name='help-contact'),
 ]
