016625212208cc33ce16f96a334332625502a162
wfdd <erdbanfed@gmail.com>
2016-04-20 16:10:31 +0300

    Shorten post labels to just the district name

--- elections/cy_2016/lib.py
+++ elections/cy_2016/lib.py
@@ -1,6 +1,4 @@
 from __future__ import unicode_literals
 
-import re
-
 def shorten_post_label(post_label):
-    return re.sub(r'^Member of Parliament for ', '', post_label)
+    return post_label.replace('βουλευτής εκλογικής περιφέρειας ', '')
