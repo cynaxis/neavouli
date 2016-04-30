27f04900fc4a11ce6f76bb76711f407920696ea6
wfdd <erdbanfed@gmail.com>
2016-04-30 19:05:35 +0300

    Fix whatever happened here

--- candidates/forms.py
+++ candidates/forms.py
@@ -43,30 +43,6 @@ else:
     StrippedCharField = forms.CharField
 
 
-
-if django_version[:2] < (1, 9):
-    class StrippedCharField(forms.CharField):
-        """A backport of the Django 1.9 ``CharField`` ``strip`` option.
-
-        If ``strip`` is ``True`` (the default), leading and trailing
-        whitespace is removed.
-        """
-
-        def __init__(self, max_length=None, min_length=None, strip=True,
-                     *args, **kwargs):
-            self.strip = strip
-            super(StrippedCharField, self).__init__(max_length, min_length,
-                                                    *args, **kwargs)
-
-        def to_python(self, value):
-            value = super(StrippedCharField, self).to_python(value)
-            if self.strip:
-                value = value.strip()
-            return value
-else:
-    StrippedCharField = forms.CharField
-
-
 class AddressForm(forms.Form):
     address = StrippedCharField(
         label=_('Enter your address or town'),
